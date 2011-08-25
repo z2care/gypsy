513672a0b880e666e4317e4db997175f9a7bde01
iain <iain@linux.intel.com>
2011-08-25 15:59:22 +0100

    Create a debugging system for Gypsy Daemon

---
+++ src/gypsy-debug.h
@@ -0,0 +1,39 @@
+#ifndef __GYPSY_DEBUG_H__
+#define __GYPSY_DEBUG_H__
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+typedef enum {
+    GYPSY_DEBUG_NMEA = 1 << 0,
+} GypsyDebugFlags;
+
+#define GYPSY_HAS_DEBUG(type) ((gypsy_debug_flags & GYPSY_DEBUG_##type) != FALSE)
+
+extern guint gypsy_debug_flags;
+
+#ifdef __GNUC__
+
+#define GYPSY_NOTE(type,x,a...) G_STMT_START {                    \
+        if (G_UNLIKELY (CLUTTER_HAS_DEBUG (type)))                      \
+            { _gypsy_message ("[" #type "] " G_STRLOC ": " x, ##a); }   \
+    } G_STMT_END
+
+#else /* !__GNUC__ */
+
+#define GYPSY_NOTE(type,...) G_STMT_START { \
+        if (G_UNLIKELY (CLUTTER_HAS_DEBUG (type))) {       \
+            char *_fmt = g_strdup_printf (__VA_ARGS__);         \
+            _gypsy_message ("[" #type "] " G_STRLOC ": %s", _fmt);      \
+            g_free (_fmt);                                              \
+        }                                                               \
+    } G_STMT_END
+
+#endif
+
+void _gypsy_message (const char *format, ...);
+
+G_END_DECLS
+
+#endif /* __GYPSY_DEBUG_H__ */
